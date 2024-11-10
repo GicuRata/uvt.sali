0dccf78669596c8b8cf9e91f6e32f9d158f08097
Gicu <gicurata@gmail.com>
2024-11-10 18:16:00 +0200

    dependencies update

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 6000;
 
 app.get('/api', (req, res) => {
     res.send('Hello from the Node.js backend!');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
